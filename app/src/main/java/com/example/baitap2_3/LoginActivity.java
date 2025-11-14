e246a48078829cd69e018e489dcb8513d9befab4
Dương Đình Ngọc Khang <Khangdz296@users.noreply.github.com>
2025-11-14 08:14:50 +0700

    Update UI Login and Signup

--- app/src/main/java/com/example/baitap2_3/LoginActivity.java
+++ app/src/main/java/com/example/baitap2_3/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.baitap2_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,12 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView reg = (TextView) findViewById(R.id.tvRegister);
+        reg.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
         });
     }
 }
